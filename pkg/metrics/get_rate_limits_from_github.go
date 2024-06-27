1ae9bb99e2cc35bf9654aa1db63d0ef7581cf168
Taylor Faubion <taylor.faubion@wbd.com>
2024-06-27 09:03:31 -0700

    use config value for determining delay between loops for rate limit check

--- pkg/metrics/get_rate_limits_from_github.go
+++ pkg/metrics/get_rate_limits_from_github.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/faubion-hbo/github-actions-exporter/pkg/config"
+
 	"github.com/google/go-github/v45/github"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -22,29 +24,27 @@ var (
 	)
 )
 
-var rateLimitsQuerySecondsBetween = 5
-
-func getRateLimits() (*github.RateLimits, int) {
+func getRateLimits() (*github.RateLimits, int64) {
 	rateLimits, resp, err := client.RateLimits(context.Background())
 
 	if rl_err, ok := err.(*github.RateLimitError); ok {
 		resetTime := rl_err.Rate.Reset.Time
-		delaySeconds := int(time.Until(resetTime).Seconds())
+		delaySeconds := int64(time.Until(resetTime).Seconds())
 		log.Printf("RateLimits ratelimited, sleeping for %ds (at %s)", delaySeconds, resetTime.String())
 		return nil, delaySeconds
 	} else if err != nil {
 		if resp != nil && resp.StatusCode == http.StatusForbidden {
 			if retryAfterSeconds, parseErr := strconv.ParseInt(resp.Header.Get("Retry-After"), 10, 32); parseErr == nil {
-				delaySeconds := int(retryAfterSeconds + (60 * rand.Int63n(randomDelaySeconds)))
+				delaySeconds := retryAfterSeconds + (60 * rand.Int63n(randomDelaySeconds))
 				log.Printf("RateLimits Retry-After %d seconds received, sleeping for %ds", retryAfterSeconds, delaySeconds)
 				return nil, delaySeconds
 			}
 		}
 		log.Printf("RateLimits error: %s", err.Error())
-		return nil, rateLimitsQuerySecondsBetween
+		return nil, config.Github.Refresh
 	}
 
-	return rateLimits, rateLimitsQuerySecondsBetween
+	return rateLimits, config.Github.Refresh
 }
 
 // getRemainingLimitsFromGithub - return information about the remaining limits for this GitHub client's credentials
